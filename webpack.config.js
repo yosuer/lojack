09c41a2619f87d23e7cbeebe74cc4be53e2534a6
yosuer <yosuer15@hotmail.com>
2018-01-13 22:43:08 -0300

    webpack a version 3 y soporte sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,23 @@
 var path = require('path');
 var node_dir = __dirname + '/node_modules';
+var webpack = require("webpack");
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    entry: './src/main/js/app.js',
+    entry: ['./src/main/js/app.js', './src/main/scss/main.scss'],
     devtool: 'sourcemaps',
     cache: true,
-    debug: true,
     resolve: {
         alias: {
             'stompjs': node_dir + '/stompjs/lib/stomp.js',
         }
     },
     output: {
-        path: __dirname,
-        filename: './src/main/resources/static/built/bundle.js'
+        filename: './src/main/resources/static/built/bundle.js',
+        path: __dirname
     },
     module: {
-        loaders: [
+        rules: [
             {
                 test: path.join(__dirname, '.'),
                 exclude: /(node_modules)/,
@@ -25,7 +26,22 @@ module.exports = {
                     cacheDirectory: true,
                     presets: ['es2015', 'react']
                 }
+            },
+            { // css / sass / scss loader for webpack
+                test: /\.(css|sass|scss)$/,
+                use: ExtractTextPlugin.extract({
+                    use: ['css-loader', 'sass-loader'],
+                })
             }
         ]
-    }
+    },
+    plugins: [
+        new webpack.LoaderOptionsPlugin({
+            debug: true
+        }),
+        new ExtractTextPlugin({ // define where to save the file
+            filename: './src/main/resources/static/built/bundle.css',
+            allChunks: true,
+        })
+    ]
 };
